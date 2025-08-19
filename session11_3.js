ce15c9b88681ffdc301b9c40fa70aa6985ba6079
Fidelia16 <nguyenphu16122002@gmail.com>
2025-08-19 10:35:51 +0900

    Create session11_3.js

---
+++ session11_3.js
@@ -0,0 +1,31 @@
+let num = +prompt ("Nhập số từ 0-9");
+switch (num) {
+        case "0":
+          text = "Số Không";
+          break;
+        case "1":
+          text = "Số Một";
+          break;
+        case "2":
+          text = "Số Hai";
+          break;
+        case "3":
+          text = "Số Ba";
+          break;
+        case "4":
+          text = "Số Bốn";
+          break;
+        case "5":
+          text = "Số Năm";
+          break;
+        case "6":
+          text = "Số Sáu";
+          break;
+        case "7":
+          text = "Số Bảy";
+          break;
+        case "8":
+          text = "Số Tám";
+          break;
+        case "9":
+          text = "Số Chín";
